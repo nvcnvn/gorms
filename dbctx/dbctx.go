1db45ef837d63d7b60b6a3358a4cd2621ccdc513
nvcnvn <nguyen@open-vn.org>
2013-02-17 02:09:35 +0700

    good

--- dbctx/dbctx.go
+++ dbctx/dbctx.go
@@ -56,6 +56,41 @@ func (ctx *DBCtx) AddMember(m *Person, id bson.ObjectId) error {
 	return ctx.personColl.Insert(m)
 }
 
+func (ctx *DBCtx) EditPerson(p *Person) error {
+	p2 := &Person{}
+	if ctx.personColl.FindId(p.PersonId).One(p2) != nil {
+		return errors.New("dbctx: non exist person data")
+	}
+
+	if p2.PersonId == p2.HouseId && (p2.Role != p.Role || p2.HouseId != p.HouseId) {
+		//The main host change their role or change their house
+		members, err := ctx.GetHouse(p2.HouseId)
+		n := len(members)
+		if err == nil && n > 1 {
+			//the house have more than 1 members.
+			//find if there is another host
+			found := false
+			for _, v := range members {
+				if v.PersonId != v.HouseId {
+					found = true
+					selector := bson.M{"houseid": v.HouseId}
+					if p2.HouseId != p.HouseId {
+						selector["_id"] = bson.M{"$ne": p.PersonId}
+					}
+
+					ctx.personColl.Update(selector, bson.M{"$set": bson.M{"houseid": v.PersonId}})
+					break
+				}
+			}
+			if !found {
+				return errors.New("dbctx: house must have another host")
+			}
+		}
+	}
+
+	return ctx.personColl.UpdateId(p.PersonId, p)
+}
+
 func (ctx *DBCtx) AddOrg(o *Organization) error {
 	o.OrgId = bson.NewObjectId()
 	return ctx.orgColl.Insert(o)
@@ -68,4 +103,109 @@ func (ctx *DBCtx) AllOrgs() ([]Organization, error) {
 		return nil, err
 	}
 	return orgs, nil
+}
+
+func (ctx *DBCtx) GetPerson(id bson.ObjectId) (*Person, error) {
+	p := &Person{}
+	if err := ctx.personColl.FindId(id).One(p); err != nil {
+		return nil, err
+	}
+	return p, nil
+}
+
+func (ctx *DBCtx) Filter(house, frole bool, role int, fqual bool, qual int,
+	fgender, gender, forg bool, orgs []bson.ObjectId, fattending, attending,
+	fworking, working, fincome bool, incomefrom, incometo int, fhi bool, hi int,
+	fhealth, health, fdesire, desire, fnote, note bool, offsetid bson.ObjectId,
+	squal, squaldesc, sage, sagedesc, sincome, sincomedesc bool, offset, limit int) ([]Person, error) {
+	plst := []Person{}
+
+	filter := bson.M{}
+
+	if fnote {
+		filter["note"] = bson.M{"$exists": note}
+	}
+
+	if fdesire {
+		filter["desire"] = bson.M{"$exists": desire}
+	}
+
+	if fhealth {
+		filter["health"] = bson.M{"$exists": health}
+	}
+
+	if fhi {
+		filter["hi"] = hi
+	}
+
+	if fincome {
+		if incometo >= incomefrom {
+			filter["avgincome"] = bson.M{
+				"$gte": incomefrom,
+				"$lte": incometo,
+			}
+		} else {
+			filter["avgincome"] = bson.M{"$gte": incomefrom}
+		}
+	}
+
+	if fworking {
+		filter["working"] = working
+	}
+
+	if fattending {
+		filter["attendingschool"] = attending
+	}
+
+	if forg {
+		filter["orgs"] = bson.M{"$in": orgs}
+	}
+
+	if frole {
+		filter["role"] = role
+	}
+
+	if fqual {
+		filter["quals"] = qual
+	}
+
+	if fgender {
+		filter["gender"] = gender
+	}
+
+	query := ctx.personColl.Find(filter)
+
+	if squal {
+		if squaldesc {
+			query.Sort("-quals")
+		} else {
+			query.Sort("quals")
+		}
+	}
+
+	if sage {
+		if sagedesc {
+			query.Sort("-birth")
+		} else {
+			query.Sort("birth")
+		}
+	}
+
+	if sincome {
+		if sincomedesc {
+			query.Sort("-avgincome")
+		} else {
+			query.Sort("avgincome")
+		}
+	}
+
+	if house {
+		query.Sort("houseid", "role", "birth")
+	}
+
+	err := query.Limit(limit).All(&plst)
+	if err != nil {
+		return nil, err
+	}
+	return plst, nil
 }
--- gorms.go
+++ gorms.go
@@ -73,6 +73,7 @@ func Data(c *Controller) {
 		m.Area = c.Post("Area", true)
 		m.Desire = c.Post("Desire", true)
 		m.Note = c.Post("Note", true)
+		m.Health = c.Post("Health", true)
 
 		m.Birth, err = time.Parse("02/01/2006", c.Post("Birth", false))
 		if err != nil {
@@ -114,14 +115,25 @@ func Data(c *Controller) {
 				m.Incomes = []dbctx.Income{dbctx.Income{amount, c.Post("Form", true)}}
 			}
 		}
-
-		if house := c.Post("house", false); bson.IsObjectIdHex(house) {
+		if person := c.Post("person", false); bson.IsObjectIdHex(person) {
+			//Edit person info
+			m.PersonId = bson.ObjectIdHex(person)
+			hid := c.Post("house", false)
+			if bson.IsObjectIdHex(hid) {
+				println("here......")
+				m.HouseId = bson.ObjectIdHex(hid)
+				m.Role, _ = strconv.Atoi(c.Post("Roles", false))
+				err := c.db.EditPerson(&m)
+				if err != nil {
+					println(err.Error())
+				}
+				c.Redirect("/data/edit.html?h="+m.HouseId.Hex(), 303)
+			}
+		} else if house := c.Post("house", false); bson.IsObjectIdHex(house) {
 			//Add member to a house
 			m.Role, _ = strconv.Atoi(c.Post("Roles", false))
 			c.db.AddMember(&m, bson.ObjectIdHex(house))
 			c.Redirect("/data/edit.html?h="+m.HouseId.Hex(), 303)
-		} else if person := c.Post("person", false); bson.IsObjectIdHex(person) {
-
 		} else {
 			//Add new host member - add new house
 			err = c.db.SaveHouse(&m)
@@ -136,13 +148,12 @@ func Data(c *Controller) {
 			}
 		}
 	} else if "/data/edit.html" == p {
-		id := c.Get("h", false)
-		if bson.IsObjectIdHex(id) {
+		if house := c.Get("h", false); bson.IsObjectIdHex(house) {
 			//House detail
-			data := c.NewViewData("View")
+			data := c.NewViewData("Thong tin ho dan")
 			data["Quals"] = Quals
 			data["Roles"] = Roles
-			h, _ := c.db.GetHouse(bson.ObjectIdHex(id))
+			h, _ := c.db.GetHouse(bson.ObjectIdHex(house))
 			data["House"] = h
 			o, _ := c.db.AllOrgs()
 			orgs := make(map[string]string)
@@ -151,6 +162,24 @@ func Data(c *Controller) {
 			}
 			data["Orgs"] = orgs
 			c.View("housedetail.tmpl", data)
+		} else if person := c.Get("p", false); bson.IsObjectIdHex(person) {
+			pData, err := c.db.GetPerson(bson.ObjectIdHex(person))
+			if err != nil {
+				c.Print("Thong tin khong ton tai.")
+				return
+			}
+
+			data := c.NewViewData("Chinh sua thogn tin")
+			o, _ := c.db.AllOrgs()
+			orgs := make(map[string]string)
+			for _, v := range o {
+				orgs[v.OrgId.Hex()] = v.Name
+			}
+			data["Orgs"] = orgs
+			data["Quals"] = Quals
+			data["Roles"] = Roles
+			data["Person"] = pData
+			c.View("personedit.tmpl", data)
 		} else {
 			//Add Host member
 			data := c.NewViewData("Add")
@@ -168,5 +197,139 @@ func Data(c *Controller) {
 		c.View("housedetail.tmpl", nil)
 	} else {
 		//List all person
+		data := c.NewViewData("Danh sach ho dan")
+		//filter parameter
+		flags := struct {
+			House, Frole, Fqual, Fgender, Fattending, Attending       bool
+			Fworking, Working, Fincome, Gender, Forg, Fhealth, Health bool
+			Fdesire, Desire, Fnote, Note, Fhi                         bool
+			Squal, Squaldesc, Sage, Sagedesc, Sincome, Sincomedesc    bool
+			Role, Qual, Incomefrom, Incometo, Hi, Offset, Limit       int
+			Orgs                                                      []bson.ObjectId
+			Offsetid                                                  bson.ObjectId
+		}{}
+		flags.Limit = 50
+		if c.Get("type", false) == "1" {
+			flags.House = true
+		}
+
+		if qual := c.Get("sort_qual", false); len(qual) == 1 {
+			flags.Squal = true
+			if qual == "1" {
+				flags.Squaldesc = true
+			}
+		}
+
+		if age := c.Get("sort_age", false); len(age) == 1 {
+			flags.Sage = true
+			if age == "1" {
+				flags.Sagedesc = true
+			}
+		}
+
+		if income := c.Get("sort_income", false); len(income) == 1 {
+			flags.Sincome = true
+			if income == "1" {
+				flags.Sincomedesc = true
+			}
+		}
+
+		if r, err := strconv.Atoi(c.Get("role", false)); err == nil {
+			flags.Role = r
+			flags.Frole = true
+		}
+
+		if q, err := strconv.Atoi(c.Get("qual", false)); err == nil {
+			flags.Qual = q
+			flags.Fqual = true
+		}
+
+		if gen := c.Get("gender", false); len(gen) == 1 {
+			flags.Fgender = true
+			if gen == "1" {
+				flags.Gender = true
+			}
+		}
+
+		if n := len(c.Request().Form["orgs"]); n > 0 {
+			flags.Orgs = make([]bson.ObjectId, 0, n)
+			for _, v := range c.Request().Form["orgs"] {
+				if bson.IsObjectIdHex(v) {
+					flags.Forg = true
+					flags.Orgs = append(flags.Orgs, bson.ObjectIdHex(v))
+				}
+			}
+		}
+
+		if a := c.Get("attending", false); len(a) == 1 {
+			flags.Fattending = true
+			if a == "1" {
+				flags.Attending = true
+			}
+		}
+
+		if w := c.Get("working", false); len(w) == 1 {
+			flags.Fworking = true
+			if w == "1" {
+				flags.Working = true
+			}
+		}
+
+		if from, err := strconv.Atoi(c.Get("incomefrom", false)); err == nil {
+			flags.Fincome = true
+			flags.Incomefrom = from
+		}
+
+		if to, err := strconv.Atoi(c.Get("incometo", false)); err == nil {
+			flags.Fincome = true
+			flags.Incometo = to
+		}
+
+		if h, err := strconv.Atoi(c.Get("hi", false)); err == nil {
+			flags.Fhi = true
+			flags.Hi = h
+		}
+
+		if h := c.Get("health", false); len(h) == 1 {
+			flags.Fhealth = true
+			if h == "1" {
+				flags.Health = true
+			}
+		}
+
+		if d := c.Get("desire", false); len(d) == 1 {
+			flags.Fdesire = true
+			if d == "1" {
+				flags.Desire = true
+			}
+		}
+
+		if n := c.Get("note", false); len(n) == 1 {
+			flags.Fnote = true
+			if n == "1" {
+				flags.Note = true
+			}
+		}
+
+		persons, _ := c.db.Filter(flags.House, flags.Frole, flags.Role,
+			flags.Fqual, flags.Qual, flags.Fgender, flags.Gender, flags.Forg,
+			flags.Orgs, flags.Fattending, flags.Attending, flags.Fworking,
+			flags.Working, flags.Fincome, flags.Incomefrom, flags.Incometo,
+			flags.Fhi, flags.Hi, flags.Fhealth, flags.Health, flags.Fdesire,
+			flags.Desire, flags.Fnote, flags.Note, flags.Offsetid, flags.Squal,
+			flags.Squaldesc, flags.Sage, flags.Sagedesc, flags.Sincome,
+			flags.Sincomedesc, flags.Offset, flags.Limit)
+		o, _ := c.db.AllOrgs()
+
+		orgsMap := make(map[string]string)
+		for _, v := range o {
+			orgsMap[v.OrgId.Hex()] = v.Name
+		}
+		data["Orgs"] = orgsMap
+		data["Quals"] = Quals
+		data["Roles"] = Roles
+		data["Persons"] = persons
+		data["Flags"] = flags
+		c.View("houselst.tmpl", data)
 	}
 }
